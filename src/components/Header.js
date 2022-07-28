6f8661a25c2dfa9b796815a5642e9edf4565d281
OhmPatil <workpatil10@gmail.com>
2022-07-29 02:08:17 +0530

    Create header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../styles/header.css'
+
+function Header(){
+    return(
+        <div id='header'>
+            <h1 id='header-title'>Memory Card</h1>
+        </div>
+    )
+}
+
+export default Header
