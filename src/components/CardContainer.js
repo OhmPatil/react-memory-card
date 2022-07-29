0228f8daa7f9e9165018f2ed5e3154aa5d026593
OhmPatil <workpatil10@gmail.com>
2022-07-29 17:35:59 +0530

    Fetch agents viaAPI

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,45 +1,47 @@
 import '../styles/card-container.css'
 import Card from './Card'
+import React, {useState, useEffect} from 'react'
 
 function CardContainer(){
 
+    console.log('CONTAINER CALLED');
+
+    const [images, setImages] = useState([])
+    const [names, setNames] = useState([])
+    const [agents, setAgents] = useState([])
+
+    const getAgents = async() => {
+        let agents = []
+
+        const url = 'https://valorant-api.com/v1/agents'
+        try {
+            const response = await fetch(url)
+            const responseData = await response.json()
+            agents = responseData.data
+        }
+        catch(err){
+            console.log(err);
+        }
+
+        return agents
+    }
+
+    useEffect(() => {
+        const loadCards = async() => {
+            setAgents(await getAgents())
+        }
+
+        loadCards()
+    }, [])
+       
+
+    // console.log(agents[0]);
+
+
     return(
         <div className='card-container'>
-            <Card imgSrc={'https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/fullportrait.png'}
-            imgAlt={'Character'}
-            cardName={'Fade'}/>
-            <Card imgSrc={'https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/fullportrait.png'}
-            imgAlt={'Character'}
-            cardName={'Fade'}/>
-            <Card imgSrc={'https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/fullportrait.png'}
-            imgAlt={'Character'}
-            cardName={'Fade'}/>
-            <Card imgSrc={'https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/fullportrait.png'}
-            imgAlt={'Character'}
-            cardName={'Fade'}/>
-            <Card imgSrc={'https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/fullportrait.png'}
-            imgAlt={'Character'}
-            cardName={'Fade'}/>
-            <Card imgSrc={'https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/fullportrait.png'}
-            imgAlt={'Character'}
-            cardName={'Fade'}/>
-            <Card imgSrc={'https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/fullportrait.png'}
-            imgAlt={'Character'}
-            cardName={'Fade'}/>
-            <Card imgSrc={'https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/fullportrait.png'}
-            imgAlt={'Character'}
-            cardName={'Fade'}/>
-            <Card imgSrc={'https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/fullportrait.png'}
-            imgAlt={'Character'}
-            cardName={'Fade'}/>
-            <Card imgSrc={'https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/fullportrait.png'}
-            imgAlt={'Character'}
-            cardName={'Fade'}/>
-            <Card imgSrc={'https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/fullportrait.png'}
-            imgAlt={'Character'}
-            cardName={'Fade'}/>
-            <Card imgSrc={'https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/fullportrait.png'}
-            imgAlt={'Character'}
+            <Card imgSrc={agents[0].fullPortrait}
+            imgAlt={'character'}
             cardName={'Fade'}/>
         </div>
     )
