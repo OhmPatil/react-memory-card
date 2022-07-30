aa7d1e575ce15f1315b2eff7e0b708e52e9a0b4f
OhmPatil <workpatil10@gmail.com>
2022-07-30 15:00:00 +0530

    Limit agents to 12 and add loading text while fetching from API

--- src/components/ApiHandler.js
+++ src/components/ApiHandler.js
@@ -1,42 +1,55 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import CardContainer from "./CardContainer";
 
 function ApiHandler() {
-    const [agents, setAgents] = useState([])
+  const [agents, setAgents] = useState([]);
+  const [loading, setLoading] = useState(false)
 
-    const getAgents = async() => {
-        let agents = []
+  const getAgents = async () => {
+    let agents = [];
 
-        const url = 'https://valorant-api.com/v1/agents'
-        try {
-            console.log('Fetching from API...');
-            const response = await fetch(url, {mode: 'cors'})
-            const responseData = await response.json()
-            agents = responseData.data
-            console.log('AGENTS: ', agents);
-        }
-        catch(err){
-            console.log('ERROR');
-            console.log(err);
-        }
-
-        return agents
+    const url = "https://valorant-api.com/v1/agents";
+    try {
+      setLoading(true)
+      console.log("Fetching from API...");
+      const response = await fetch(url, { mode: "cors" });
+      const responseData = await response.json();
+      agents = responseData.data;
+      console.log("AGENTS while fetching: ", agents);
+    } catch (err) {
+      console.log("ERROR");
+      console.log(err);
     }
 
-    useEffect(() => {
-        const loadCards = async() => {
-            setAgents(await getAgents())
+    return agents;
+  };
+
+  useEffect(() => {
+    const loadCards = async () => {
+      const allAgents = await getAgents();
+      let reqAgents = [];
+      console.log("ALL AGENTS: ", allAgents);
+
+      for (let i = 0; i <= 12; i++) {
+        if (allAgents[i].isPlayableCharacter) {
+          reqAgents.push(allAgents[i]);
         }
+      }
 
-        loadCards()
-    }, [])
+      console.log("REQUIRED AGENTS: ", reqAgents);
+      setAgents(reqAgents);
+      setLoading(false)
+    };
 
-    return(
-        <>
-            <CardContainer agents={agents}/>
-        </>
-    )
-       
+    loadCards();
+  }, []);
+
+  return (
+    <>
+       {loading && (<CardContainer loading={true} agents={agents}/>)}
+      <CardContainer agents={agents} />
+    </>
+  );
 }
 
 export default ApiHandler;
--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,13 +1,25 @@
 import '../styles/card-container.css'
 import Card from './Card'
+// import React, {useState} from 'react';
 
 function CardContainer(props){
 
     console.log('CONTAINER CALLED');
 
+    // const [localAgents, setLocalAgents] = useState([])
+
+    function shuffleContainer(array){
+        return [...array].sort(() => Math.random() - 0.5)
+    }
+
+
     return(
         <div className='card-container'>
-            {props.agents.map(agent => {
+            {props.loading && (
+                <>LOADING...</>
+            )}
+
+            {shuffleContainer(props.agents).map(agent => {
                 return (
                     <Card
                     key={agent.uuid}
