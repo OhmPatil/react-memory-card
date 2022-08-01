58a6fe03a6a7d8b133d5d8bcf1d17fd462d4bbd3
OhmPatil <workpatil10@gmail.com>
2022-08-01 14:29:27 +0530

    Complete footer, add react-icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import '../styles/footer.css'
+import {FaGithub} from 'react-icons/fa'
 
 function Footer(){
     return(
         <div id='footer'>
-            <p id='footer-text'>Made by Ohm Patil</p>
+            <p id='footer-text'>Made by Ohm Patil<a href='https://github.com/OhmPatil' target='_blank' rel='noopener noreferrer'><FaGithub size={'1.5rem'}/></a></p>
         </div>
     )
 }
