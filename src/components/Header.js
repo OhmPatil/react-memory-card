e3f639b7ac212d0cae5b8f770b6e32c9acdf1df8
OhmPatil <workpatil10@gmail.com>
2022-08-01 11:53:13 +0530

    Change name from memory card => memory game

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import '../styles/header.css'
 function Header(){
     return(
         <div id='header'>
-            <h1 id='header-title'>Memory Card</h1>
+            <h1 id='header-title'>Memory Game</h1>
         </div>
     )
 }
